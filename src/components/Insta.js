5089d9c53bd9d130c0ad2ce90577425bf966ba0d
Florenz Austria <florenzdalangin@gmail.com>
2022-04-07 15:33:12 -0400

    organize grid

--- src/components/Insta.js
+++ src/components/Insta.js
@@ -4,27 +4,28 @@ import "../css/Insta.css"
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 
-import Img1 from '../assets/gridimg/imga.png'
-import Img2 from '../assets/gridimg/imgb.png'
-import Img3 from '../assets/gridimg/imgc.png'
-import Img4 from '../assets/gridimg/imgd.png'
-import Img5 from '../assets/gridimg/imge.png'
-import Img6 from '../assets/gridimg/imgf.jpg'
-import Img7 from '../assets/gridimg/imgg.png'
+// import Img1 from '../assets/gridimg/imga.png'
+// import Img2 from '../assets/gridimg/imgb.png'
+// import Img3 from '../assets/gridimg/imgc.png'
+// import Img4 from '../assets/gridimg/imgd.png'
+// import Img5 from '../assets/gridimg/imge.png'
+// import Img6 from '../assets/gridimg/imgf.jpg'
+// import Img7 from '../assets/gridimg/imgg.png'
 
-import Alt1 from '../assets/gridimg/alta.jpg'
-import Alt2 from '../assets/gridimg/altb.jpg'
-import Alt3 from '../assets/gridimg/altc.jpg'
-import Alt4 from '../assets/gridimg/altd.jpg'
-import Alt5 from '../assets/gridimg/alte.png'
-import Alt6 from '../assets/gridimg/altf.jpg'
+// import Alt1 from '../assets/gridimg/alta.jpg'
+// import Alt2 from '../assets/gridimg/altb.jpg'
+// import Alt3 from '../assets/gridimg/altc.jpg'
+// import Alt4 from '../assets/gridimg/altd.jpg'
+// import Alt5 from '../assets/gridimg/alte.png'
+// import Alt6 from '../assets/gridimg/altf.jpg'
 
+import IpadButton from "../assets/ipadbutton.png"
 import LoremA from '../assets/gridimg/lorem.png'
 import LoremB from '../assets/gridimg/lorem2.png'
 
 export default function Insta() {
-    const imgs = [Img1, Img2, Img3, Img4, Img5, Img6, Img7] //imgs[index]
-
+    // const imgs = [Img1, Img2, Img3, Img4, Img5, Img6, Img7] //imgs[index]
+    // const alts = [Alt1, Alt2, Alt3, Alt4, Alt5, Alt6, Alt5] 
     const startImgs = [
         {
             name: "Name",
@@ -144,8 +145,9 @@ export default function Insta() {
     ]
 
     const quotes = ["Herrow", "Herrow", "Herrow", "Herrow", "Herrow", "Herrow", "Herrow", "Herrow", "Herrow", "Herrow", "Herrow", "Herrow"]
-    const alts = [Alt1, Alt2, Alt3, Alt4, Alt5, Alt6, Alt5] 
+    
     const [items, setItems] = useState(startImgs)
+    const [isOn, setIsOn] = useState(false);
 
     const handleClick = (i) => {
         const currItem = {...items[i]["style"]}
@@ -161,13 +163,17 @@ export default function Insta() {
         setItems(newItems)
     }
 
+    const onIpad = () => {
+        setIsOn(!isOn);
+        console.log("Click!!")
+    }
+
     return(
         <div id="insta" className="insta-sec">
-            <h1 className="insta-title">Grid Page</h1>
+            {/* <h1 className="insta-title">Grid Page</h1> */}
             <div className="ipad">
                 <div className="in-ipad">
                     <Box sx={{ flexGrow: 1 }} className="ipad-box">
-                        
                         <Grid container columns={{ xs: 4, lg: 6}} className="quote-grid">
                             {quotes.map((_, index) => (
                             <Grid item xs={2} key={index} className="ipad-quote">
@@ -188,9 +194,13 @@ export default function Insta() {
                             </Grid>
                             ))}
                         </Grid>
-
-                        
                     </Box>
+                </div>
+
+                <div className={isOn ? "off-ipad" : null}></div>
+
+                <div className="" onClick={() => {onIpad()}}>
+                    <img src={IpadButton} alt="ipad button" className="ipad-button"/>
                 </div>
             </div>
         </div>
